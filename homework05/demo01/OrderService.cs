a0051e2a5be074a5ab9dc8208191545dd1b28624
wzk <1403235458@qq.com>
2021-04-02 08:33:10 +0800

    增加订单添加是否相等判断

--- homework05/demo01/OrderService.cs
+++ homework05/demo01/OrderService.cs
@@ -9,19 +9,22 @@ namespace demo01
     class OrderService
     {
        public List<Order> orderList = new List<Order>();
-        public void AddOrder(Order order) { orderList.Add(order); }
+        public void AddOrder(Order order) {
+            orderList.ForEach(item => { if (order.Equals(item)) throw new ApplicationException("订单已存在"); });
+            orderList.Add(order); 
+        }
         public void DeleteOrder(Int32 orderNoNum)
         {
             if (orderList.Remove(orderList.Find(order => order.OrderNo==orderNoNum))) Console.WriteLine("删除成功");
             else throw new ArgumentException("删除失败");
         }
-        public List<Order> QueryOrder(Int32 orderNoNum) {
+        public Order QueryOrder(Int32 orderNoNum) {
             List<Order> queryList = (from order 
                                      in orderList 
                                      where orderNoNum == order.OrderNo 
                                      select order).ToList();
             
-            return queryList;
+            return queryList.First();
         }
         public List<Order> QueryOrder(String commodityName)
         {
