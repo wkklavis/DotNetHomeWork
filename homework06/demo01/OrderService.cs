776f1babf070863a7bf7a7f3881961bdbe3c2594
wzk <1403235458@qq.com>
2021-04-02 08:37:42 +0800

    更改

--- homework06/demo01/OrderService.cs
+++ homework06/demo01/OrderService.cs
@@ -10,36 +10,56 @@ namespace demo01
     class OrderService
     {
         public List<Order> orderList = new List<Order>();
-       // XmlSerializer xmlSerializer = new XmlSerializer(typeof(Order));
-        public void AddOrder(Order order) { orderList.Add(order); }
+        public void AddOrder(Order order)
+        {
+            orderList.ForEach(item => { if (order.Equals(item)) throw new ApplicationException("订单已存在"); });
+            orderList.Add(order);
+        }
         public void DeleteOrder(Int32 orderNoNum)
         {
-            if (orderList.Remove(orderList.Find(order => order.OrderNo==orderNoNum))) Console.WriteLine("删除成功");
+            if (orderList.Remove(orderList.Find(order => order.OrderNo == orderNoNum))) Console.WriteLine("删除成功");
             else throw new ArgumentException("删除失败");
         }
-        public List<Order> QueryOrder(Int32 orderNoNum) {
-            List<Order> queryList = (from order in orderList where orderNoNum == order.OrderNo select order).ToList();
-            
-            return queryList;
+        public Order QueryOrder(Int32 orderNoNum)
+        {
+            List<Order> queryList = (from order
+                                     in orderList
+                                     where orderNoNum == order.OrderNo
+                                     select order).ToList();
+
+            return queryList.First();
         }
         public List<Order> QueryOrder(String commodityName)
         {
-            List<Order> queryList = (from order in orderList where order.list.Find(item=>item.CommodityName==commodityName)!=null orderby order.totalPrice  select order  ).ToList();
-            
+            List<Order> queryList = (from order
+                                     in orderList
+                                     where order.list.Find(item => item.CommodityName == commodityName) != null
+                                     orderby order.totalPrice
+                                     select order).ToList();
+
             return queryList;
         }
         public List<Order> QueryOrder(Customer customer)
         {
-            List<Order> queryList = (from order in orderList where order.customer== customer orderby order.totalPrice select order).ToList();
-            
+            List<Order> queryList = (from order
+                                     in orderList
+                                     where order.customer == customer
+                                     orderby order.totalPrice
+                                     select order).ToList();
+
             return queryList;
         }
         public List<Order> QueryOrder(double totalPrice)
         {
-            List<Order> queryList = (from order in orderList where order.totalPrice == totalPrice orderby order.totalPrice select order).ToList();
-           
+            List<Order> queryList = (from order
+                                     in orderList
+                                     where order.totalPrice == totalPrice
+                                     orderby order.totalPrice
+                                     select order).ToList();
+
             return queryList;
         }
+
 
         //添加Export以及Import方法
         public void Export()
